52c85b83e4c86ad74ada9410f975e11b81db786f
daniock <dshchepetilnikov@gmail.com>
2020-10-16 15:41:51 +0300

    Implement main jobrun functionality

---
+++ jobrun/__init__.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+
+__version__ = '1.0'
+
+def read_yaml(path) -> dict:
+    with open(path, 'r') as file:
+        content = file.read()
+    return yaml.safe_load(content)
+
+class Runner:
+    def __init__(self, yaml: dict):
+        self.__yaml = yaml
+
+    def __run_script(self, script: list):
+        command = ' && '.join(script)
+        os.system(command)
+
+    def before_script(self):
+        before_script = self.__yaml['before_script']
+        self.__run_script(before_script)
+
+    def job(self, name: str):
+        script = self.__yaml[name]['script']
+        self.__run_script(script)
